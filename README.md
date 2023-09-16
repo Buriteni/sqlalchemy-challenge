ceb1f37fdbdb5a3b4fb50636a37a9610be1931ff
Brittney Hill <britthillre@gmail.com>
2023-09-15 22:30:48 -0500

    Read

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # sqlalchemy-challenge
-read me
+## Introduction
+
+Congratulations! You've decided to treat yourself to a long holiday vacation in Honolulu, Hawaii. To help with your trip planning, you decide to do a climate analysis about the area. The following sections outline the steps that you need to take to accomplish this task.
+
+## Part 1: Analyze and Explore the Climate Data
+
+In this section, I used Python and SQLAlchemy to do a basic climate analysis and data exploration of your climate database. Specifically, used SQLAlchemy ORM queries, Pandas, and Matplotlib. 
+
+1. Note that you’ll use the provided files (climate_starter.ipynb and hawaii.sqlite) to complete your climate analysis and data exploration.<br/>
+2. Use the SQLAlchemy create_engine() function to connect to your SQLite database.<br />
+3. Use the SQLAlchemy automap_base() function to reflect your tables into classes, and then save references to the classes named station and measurement.
+4. Link Python to the database by creating a SQLAlchemy session.
+5. Perform a precipitation analysis and then a station analysis by completing the steps in the following two subsections.
+
+### Precipitation Analysis
+
+1. Find the most recent date in the dataset.
+2. Using that date, get the previous 12 months of precipitation data by querying the previous 12 months of data.
+3. Select only the "date" and "prcp" values.
+4. Load the query results into a Pandas DataFrame. Explicitly set the column names.
+5. Sort the DataFrame values by "date".
+
+
+
+
--- app.py
+++ app.py
@@ -1,30 +1,58 @@
 # Import the dependencies.
-
-
+from flask import Flask
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+from sqlalchemy.orm import Session
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
 
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+@app.route("/")
+def  welcome():
+   """""List all available api routes."""""
+   return (
+      f"/api/v1.0/precipitation"
+      f"/api/v1.0/stations"
+      f"/api/v1.0/tobs"
+      f"/api/v1.0/<start>"
+      f"/api/v1.0/<start>/<end>"
+   )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+   session = Session(engine)
+   """""Precipitation last 13 months."""""
+   last_date = session.query(measurement.date, measurement.prcp).\
+   filter(measurement.date >= one_year_ago).all()
+   """""Convert the query result to a dictionary using date as the key and prcp as the value"""""
+prcp_dict = []
+for row in prcp_query:
+        prcp_dict[row.date] = row.prcp
+"""""Return JSON"""""
+return jsonify(prcp_dict)
+
+   
+
+
